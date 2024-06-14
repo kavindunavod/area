a630d5968677249768bbdf1133c26cee4820393b
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-14 21:02:37 +0530

    area of a circle

---
+++ project2.scala
@@ -0,0 +1,17 @@
+
+object project2 {
+
+  def main(args: Array[String]) : Unit = {
+
+  println("enter the radius value: ");
+    val radius = 5;
+    val pi= 3.14;
+    val area = pi * radius * radius;
+    println("area is: " + area);
+
+
+
+  }
+
+
+}
