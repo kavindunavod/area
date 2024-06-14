4792704f91e6938472af7e31b666a13c20f65542
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-15 00:08:36 +0530

    Update project2.scala

--- project2.scala
+++ project2.scala
@@ -1,17 +1,16 @@
 
-object project2 {
+object project4 {
 
   def main(args: Array[String]) : Unit = {
 
-  println("enter the radius value: ");
-    val radius = 5;
-    val pi= 3.14;
-    val area = pi * radius * radius;
-    println("area is: " + area);
-
-
+  calculate();
 
   }
+  def calculate(): Unit = {
+    val temp = 35;
+    val farah = temp * 1.800 + 32.00;
+    println("fahrenheit of 35c is " + farah + "f");
 
+  }
 
 }
